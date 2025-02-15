a00c3b3191567dc90d712cdd99fac8279238b5a6
Saransh Agarwal <95467072+Saransh-2403@users.noreply.github.com>
2025-02-15 12:19:40 +0530

    numbered grammar symbol

--- include/parserDef.h
+++ include/parserDef.h
@@ -2,118 +2,139 @@
 #ifndef PARSERDEF_H
 #define PARSERDEF_H
 
+#include<vectorofvector.h>
+struct FirstAndFollow
+{
+    int **first;
+    int **follow;
+};
+
+struct grammar
+{
+    VectorOfVector *Grammar;
+};
+
+typedef struct grammar *grammar;
+typedef struct FirstAndFollow *FirstAndFollow;
+
+#define NONTERMINALS 53
+#define TERMINALS 57
+#define EPSN 110
+
 static char *grammarTerms[111] = {
-    "$",                         // 0
-    "program",                   // 1
-    "otherFunctions",            // 2
-    "mainFunction",              // 3
-    "stmts",                     // 4
-    "function",                  // 5
-    "input_par",                 // 6
-    "output_par",                // 7
-    "parameter_list",            // 8
-    "dataType",                  // 9
-    "primitiveDataType",         // 10
-    "constructedDataType",       // 11
-    "A",                         // 12
-    "typeDefinitions",           // 13
-    "declarations",              // 14
-    "otherStmts",                // 15
-    "returnStmt",                // 16
-    "typeDefinition",            // 17
-    "definetypestmt",            // 18
-    "fieldDefinitions",          // 19
-    "fieldDefinition",           // 20
-    "moreFields",                // 21
-    "global_or_not",             // 22
-    "stmt",                      // 23
-    "assignmentStmt",            // 24
-    "iterativeStmt",             // 25
-    "conditionalStmt",           // 26
-    "ioStmt",                    // 27
-    "funCallStmt",               // 28
-    "SingleOrRecId",             // 29
-    "arithmeticExpression",      // 30
-    "remaining_list",            // 31
-    "option_single_constructed", // 32
-    "oneExpansion",              // 33
-    "moreExpansions",            // 34
-    "outputParameters",          // 35
-    "inputParameters",           // 36
-    "idList",                    // 37
-    "booleanExpression",         // 38
-    "factorConditionalStmt",     // 39
-    "var",                       // 40
-    "arith_term",                // 41
-    "arithmetic_recursion",      // 42
-    "operator",                  // 43
-    "factor",                    // 44
-    "term_recursion",            // 45
-    "operator_second",           // 46
-    "declaration",               // 47
-    "logicalOp",                 // 48
-    "relationalOp",              // 49
-    "optionalReturn",            // 50
-    "more_ids",                  // 51
-    "booleanTerm",               // 52
-    "TK_COMMENT",                // 53
-    "TK_FIELDID",                // 54
-    "TK_ID"                     // 55
-    "TK_NUM",                    // 56
-    "TK_RNUM",                   // 57
-    "TK_FUNID",                  // 58
-    "TK_RUID",                   // 59
-    "TK_WITH",                   // 60
-    "TK_PARAMETERS",             // 61
-    "TK_END",                    // 62
-    "TK_WHILE",                  // 63
-    "TK_UNION",                  // 64
-    "TK_ENDUNION",               // 65
-    "TK_DEFINETYPE",             // 66
-    "TK_AS",                     // 67
-    "TK_TYPE",                   // 68
-    "TK_MAIN",                   // 69
-    "TK_GLOBAL",                 // 70
-    "TK_PARAMETER",              // 71
-    "TK_LIST",                   // 72
-    "TK_SQL",                    // 73
-    "TK_SQR",                    // 74
-    "TK_INPUT",                  // 75
-    "TK_OUTPUT",                 // 76
-    "TK_INT",                    // 77
-    "TK_REAL",                   // 78
-    "TK_COMMA",                  // 79
-    "TK_SEM",                    // 80
-    "TK_COLON",                  // 81
-    "TK_DOT",                    // 82
-    "TK_ENDWHILE",               // 83
-    "TK_OP",                     // 84
-    "TK_CL",                     // 85
-    "TK_IF",                     // 86
-    "TK_THEN",                   // 87
-    "TK_ENDIF",                  // 88
-    "TK_READ",                   // 89
-    "TK_WRITE",                  // 90
-    "TK_RETURN",                 // 91
-    "TK_PLUS",                   // 92
-    "TK_MINUS",                  // 93
-    "TK_MUL",                    // 94
-    "TK_DIV",                    // 95
-    "TK_CALL",                   // 96
-    "TK_RECORD",                 // 97
-    "TK_ENDRECORD",              // 98
-    "TK_ELSE",                   // 99
-    "TK_AND",                    // 100
-    "TK_OR",                     // 101
-    "TK_NOT",                    // 102
-    "TK_LT",                     // 103
-    "TK_LE",                     // 104
-    "TK_EQ",                     // 105
-    "TK_GT",                     // 106
-    "TK_GE",                     // 107
-    "TK_NE",                     // 108
-    "TK_ASSIGNOP",               // 109
-    "eps"                        // 110
+"$",                         // 0
+//terminals
+"TK_MAIN",                   // 1
+"TK_END",                    // 2
+"TK_FUNID",                  // 3
+"TK_SEM",                    // 4
+"TK_INPUT",                  // 5
+"TK_PARAMETER",              // 6
+"TK_LIST",                   // 7
+"TK_SQL",                    // 8
+"TK_SQR",                    // 9
+"TK_OUTPUT",                 // 10
+"TK_INT",                    // 11
+"TK_REAL",                   // 12
+"TK_RUID",                   // 13
+"TK_COMMA",                  // 14
+"TK_RECORD",                 // 15
+"TK_ENDRECORD",              // 16
+"TK_UNION",                  // 17
+"TK_ENDUNION",               // 18
+"TK_TYPE",                   // 19
+"TK_COLON",                  // 20
+"TK_FIELDID",                // 21
+"TK_GLOBAL",                 // 22
+"TK_ASSIGNOP",               // 23
+"TK_WHILE",                  // 24
+"TK_OP",                     // 25
+"TK_CL",                     // 26
+"TK_IF",                     // 27
+"TK_THEN",                   // 28
+"TK_ELSE",                   // 29
+"TK_ENDIF",                  // 30
+"TK_READ",                   // 31
+"TK_WRITE",                  // 32
+"TK_PLUS",                   // 33
+"TK_MINUS",                  // 34
+"TK_MUL",                    // 35
+"TK_DIV",                    // 36
+"TK_NOT",                    // 37
+"TK_AND",                    // 38
+"TK_OR",                     // 39
+"TK_LT",                     // 40
+"TK_LE",                     // 41
+"TK_EQ",                     // 42
+"TK_GT",                     // 43
+"TK_GE",                     // 44
+"TK_NE",                     // 45
+"TK_RETURN",                 // 46
+"TK_DEFINETYPE",             // 47
+"TK_AS",                     // 48
+"TK_DOT",                    // 49
+"TK_CALL",                   // 50
+"TK_WITH",                   // 51
+"TK_PARAMETERS",             // 52
+"TK_NUM",                    // 53
+"TK_RNUM",                   // 54
+"TK_ENDWHILE",               // 55
+"TK_ID",                     // 56
+// Non-Terminals
+"program",                   // 57
+"otherFunctions",            // 58
+"mainFunction",              // 59
+"function",                  // 60
+"input_par",                 // 61
+"output_par",                // 62
+"parameter_list",            // 63
+"dataType",                  // 64
+"primitiveDataType",         // 65
+"constructedDataType",       // 66
+"remaining_list",            // 67
+"stmts",                     // 68
+"typeDefinitions",           // 69
+"actualOrRedefined",         // 70
+"typeDefinition",            // 71
+"fieldDefinitions",          // 72
+"fieldDefinition",           // 73
+"fieldType",                 // 74
+"moreFields",                // 75
+"declarations",              // 76
+"declaration",               // 77
+"global_or_not",             // 78
+"otherStmts",                // 79
+"stmt",                      // 80
+"assignmentStmt",            // 81
+"SingleOrRecId",             // 82
+"option_single_constructed", // 83
+"oneExpansion",              // 84
+"moreExpansions",            // 85
+"funCallStmt",               // 86
+"outputParameters",          // 87
+"inputParameters",           // 88
+"iterativeStmt",             // 89
+"conditionalStmt",           // 90
+"elsePart",                  // 91
+"ioStmt",                    // 92
+"arithmeticExpression",      // 93
+"expPrime",                  // 94
+"term",                      // 95
+"termPrime",                 // 96
+"factor",                    // 97
+"lowPrecedenceOp",           // 98
+"highPrecedenceOp",          // 99
+"booleanExpression",         // 100
+"var",                       // 101
+"logicalOp",                 // 102
+"relationalOp",              // 103
+"returnStmt",                // 104
+"optionalReturn",            // 105
+"idList",                    // 106
+"more_ids",                  // 107
+"definetypestmt",            // 108
+"A",                         // 109
+"eps"                        // 110
 };
 
 #endif
